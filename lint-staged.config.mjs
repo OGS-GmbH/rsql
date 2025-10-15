e6e557f6254e470047a5b83f5387b6ac97515d4d
Elie Soueid <eliegebransoued@gmail.com>
2025-10-15 10:24:19 +0200

    fix: fix linting issues

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 export default {
   "*.{ts,js,mjs,cjs}": "eslint",
   "package.json":
-    "npmPkgJsonLint -c ./node_modules/@ogs-gmbh/linter/package-json-open-source.rules.json",
+    "npmPkgJsonLint -c ./node_modules/@ogs-gmbh/linter/package-json-open-source.rules.json"
 };
